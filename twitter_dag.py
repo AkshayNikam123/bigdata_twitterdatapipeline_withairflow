e226b61a1a3862133f52b28466d4fdd4ffd87b23
Akshay Nikam <108882996+AkshayNikam123@users.noreply.github.com>
2023-12-01 12:03:00 +0530

    Add files via upload

---
+++ twitter_dag.py
@@ -0,0 +1,34 @@
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from datetime import datetime
+from twitter_etl import run_twitter_etl
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2020, 11, 8),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+#creating dag here 
+dag = DAG(
+    'twitter_dag', #dag name
+    default_args=default_args, #above args
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1),
+)
+
+#using python opeartor to run dag
+run_etl = PythonOperator(
+    task_id='complete_twitter_etl', #any name
+    python_callable=run_twitter_etl, #function we define in twiiter_etl.py
+    dag=dag, 
+)
+
+run_etl
---
+++ twitter_etl.py
@@ -0,0 +1,53 @@
+import tweepy
+import pandas as pd 
+import json
+from datetime import datetime
+import s3fs 
+
+def run_twitter_etl():
+
+
+    access_key = "WcTqgRxbKGSw2NzxFwCDZZWLf"  #api key
+    access_secret = "k5LYTsWGq5JQ8IDPRL30mCavipwMxxT2dnrJK2FAhJ0qSq8nEd"  #api key secret
+    consumer_key = "2956253635-BbebR9oCx5Ipu75LroSzN9VPvA9yS0nVoEX9NVn"  #acces token
+    consumer_secret = "wBDCzmGlzzLTpUbZBmsnNV3zTmr4iYlBnkqfU3HrNSvjT"    #acees token secret
+     
+
+
+    # Twitter authentication
+    auth = tweepy.OAuthHandler(access_key, access_secret)   #refer tweepy documentation
+    auth.set_access_token(consumer_key, consumer_secret) 
+
+    # # # Creating an API object 
+    api = tweepy.API(auth)  #create ojcet for api pass varibale like keys 
+    tweets = api.user_timeline(screen_name='@iamsrk', 
+                            # 200 is the maximum allowed count
+                            count=200,
+                            include_rts = False,    
+                            #for retweets variable
+                            # Necessary to keep full_text 
+                            # otherwise only the first 140 words are extracted
+                            tweet_mode = 'extended'
+                            )
+   
+
+
+    list = []
+    for tweet in tweets:
+        text = tweet._json["full_text"]
+
+        refined_tweet = {"user": tweet.user.screen_name,
+                        'text' : text,
+                        'favorite_count' : tweet.favorite_count,
+                        'retweet_count' : tweet.retweet_count,
+                        'created_at' : tweet.created_at}
+        
+        list.append(refined_tweet)
+
+    df = pd.DataFrame(list)
+    df.to_csv('s3://akshaytweet/srk.csv') 
+    #after creating bucket just copy bucket name and give file you want to save data
+    #bucket  akshaytweet
+    #file srk
+
+ 
